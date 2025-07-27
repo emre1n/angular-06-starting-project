8e71eb6c2cbb611c2511ffefe66fc64809804d54
emre1n <emre1n@protonmail.com>
2025-07-27 22:46:31 +0300

    add TemperaturePipe starter as a custom pipe

---
+++ src/app/temperature.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temp',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(value: number) {
+    return value + '°C';
+  }
+}
